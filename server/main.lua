e4535a7fbec7f4903499e91bb737269a48f22131
Kamkus <37803124+Kamkus@users.noreply.github.com>
2021-05-01 22:35:32 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,5 @@
   
+  
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
@@ -18,6 +19,33 @@ RegisterNetEvent("AddTable")
 AddEventHandler("AddTable", function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.addInventoryItem('table', 1)
+end)
+
+RegisterNetEvent("Sell")
+AddEventHandler("Sell", function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	amount = xPlayer.getInventoryItem('meth').count
+	print("xDDDD")
+	local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {["@identifier"] = xPlayer.identifier})
+	if result[1].lvl == 1 or result[1].lvl == 2 then
+		TriggerClientEvent("methjob_notify", _source, "This is some shitty stuff. I will pay maximum " .. Config.prices.lvl1 ..  " per each Meth", "info")
+		xPlayer.addMoney(amount* Config.prices.lvl1)
+		TriggerClientEvent("methjob_notify", _source, "You sold! " .. amount ..  " meth for " .. amount* Config.prices.lvl1 .. " $", "Success")
+	elseif result[1].lvl == 3 or result[1].lvl == 4 then
+		TriggerClientEvent("methjob_notify", _source, "This isn't the worst but isn't the best stuff. I will pay " .. Config.prices.lvl3 ..  " per each Meth", "info")
+		xPlayer.addMoney(amount* Config.prices.lvl3)
+		TriggerClientEvent("methjob_notify", _source, "You sold! " .. amount ..  " meth for " .. amount* Config.prices.lvl3 .. " $", "Success")
+	elseif result[1].lvl == 5 or result[1].lvl == 6 or result[1].lvl == 7 or result[1].lvl == 8 then
+		TriggerClientEvent("methjob_notify", _source, "This is good men! I like this stuff. I will pay " .. Config.prices.lvl5 ..  " per each Meth", "info")
+		xPlayer.addMoney(amount* Config.prices.lvl5)
+		TriggerClientEvent("methjob_notify", _source, "You sold! " .. amount ..  " meth for " .. amount* Config.prices.lvl5 .. " $", "Success")
+	else
+		TriggerClientEvent("methjob_notify", _source, "This stuff is fukkin amazing! I love it men! " .. Config.prices.lvl10 ..  " per each Meth", "info")
+		xPlayer.addMoney(amount* Config.prices.lvl10)
+		TriggerClientEvent("methjob_notify", _source, "You sold! " .. amount ..  " meth for " .. amount* Config.prices.lvl10 .. " $", "Success")
+	end
+	xPlayer.removeInventoryItem('meth', amount)
 end)
 
 RegisterNetEvent("Update")
