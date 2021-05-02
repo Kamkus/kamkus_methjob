9dfeffcfcb6817f329f335e0249759419aeb7dff
Kamkus <37803124+Kamkus@users.noreply.github.com>
2021-05-02 15:37:30 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.amount =
 
 Config.count =
 {
-	count = 7, -- How many events will show before end
+	count = 3, -- How many events will show before end
 }
 
 Config.time = 
@@ -18,4 +18,36 @@ Config.time =
 Config.chance = 
 {
 	chance = 20 --in %.Chance to explode after not answering or bad Answer
+}
+
+Config.prices = 
+{
+    lvl1 = 100,
+    lvl3 = 300,
+    lvl5 = 600,
+    lvl10 = 1000
+}
+
+Config.DealerPosition = 
+{
+    vector3(2136.1149902344, 4774.7329101563, 39.970275878906),
+}
+
+Config.CarSpawnPosition = 
+{
+    vector3(2125.8171386719,  4797.3530273438 , 41.141143798828),
+}
+
+Config.DeliveryPoints = 
+{
+   vector3(1694.587, 3280.717 , 41.127),
+   vector3(-1193.865,  -2930.36 , 13.945),
+   vector3(1398.23, 1119.537,114.838),
+   vector3(2413.722,4992.176,46.264),
+   vector3(1374.544, 3617.478, 34.892),
+   vector3(100.565, 3743.735, 39.742),
+   vector3(3328.939, 5154.258, 18.303),
+   vector3(-283.901, 2537.594, 74.668),
+   vector3(1181.474, -3272.419, 5.529)
+
 }
